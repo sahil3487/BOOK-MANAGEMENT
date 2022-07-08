59d0f1de3aaa760dd47d6a8c65d4b074c0e06dbd
Sarhank44 <you@example.com>
2022-07-08 16:28:09 +0530

    review

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -11,26 +11,18 @@ let createReview = async (req, res) => {
         if (!mongoose.Types.ObjectId.isValid(bookId)) return res.status(400).send({ status: false, msg: 'please enter the bookId' });
 
         let findBook = await bookModel.findOne({ _id: bookId, isDeleted: false });
-        if (!findBook) return res.status(404).send({ status: false, msg: 'bookId does not exists' })
+        if (!findBook) return res.status(404).send({ status: false, msg: 'book does not exists' })
 
         let { reviewedBy, reviewedAt, rating, review } = data;
 
-        let filter = { isDeleted: false };
+        reviewedAt = new Date()
 
-        if (!reviewedBy) return res.status(400).send({ status: false, msg: 'please enter reviewedby' });
-        if (typeof reviewedBy != "string") return res.status(400).send({ status: false, msg: 'please enter valid reviewers name' })
+        if (!rating) return res.status(400).send({ status: false, msg: 'rating is required' });
+        if(!(rating > 0 && rating <= 5)) return res.status(200).send({ status: false, msg: 'please enter valid rating which is more than 0 and less than or equal to 5'})
 
-        if (!reviewedAt) return res.status(400).send({ status: false, msg: 'please enter reviewed date' });
-        let validateDate = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/gm
-        if (!validateDate.test(reviewedAt)) {
-            return res.status(400).send({ status: false, message: "reviewed date must be in format  YYYY-MM-DD!!!" })
-        }
-
-        if (!rating) return res.status(400).send({ status: false, msg: 'please enter valid rating which is more than 0 and less than or equal to 5' });
-
-        filter = {
+        let filter = {
             bookId: bookId,
-            reviewedBy: reviewedBy,
+            reviewedBy,
             reviewedAt: reviewedAt,
             rating: rating,
             review: review
@@ -39,14 +31,17 @@ let createReview = async (req, res) => {
         findBook.reviews = findBook.reviews + 1;
         findBook.save();
 
-        await reviewModel.create(filter);
+        let savedData = await reviewModel.create(filter);
 
-        let response = await reviewModel.findOne(filter).select({__v:0,updatedAt:0,createdAt:0,isDeleted:0})
+        let response = await reviewModel.findById(savedData._id).select({__v:0,isDeleted:0})
 
         res.status(201).send({ status: true, msg: 'success', data: response })
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
     }
 }
+
+
+
 
 module.exports.createReview = createReview
