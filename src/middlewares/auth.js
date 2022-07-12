abcf129b4e68014c719441fcb2777ed3376b9971
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-07-12 12:26:22 +0530

    finishing touch

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,21 +9,17 @@ const authenticate = async (req, res, next) => {
         if (!token) return res.status(400).send({ status: false, message: "token must be present", });
 
         //-----(Decoding Token)
-        let decodedToken = jwt.verify(token, "project-3")
-
-        if(!decodedToken) return res.status(401).send({status: false,message: 'token invalid'})
-
-        //----(Set Id In Request)
-        req["userId"] = decodedToken.userId
-    
-        next()
+        jwt.verify(token, "project-3", (error, decoded) => {
+            if (error) {
+                return res.status(401).send({ status: false, message: error.message })
+            } 
+            else {
+                req["userId"] = decoded.userId;
+                next()
+            }
+        })
         
     } catch (err) {
-
-        if (err.message == "jwt expired") return res.status(400).send({ status: false, message: "JWT expired, login again" })
-        if (err.message == "invalid signature") return res.status(400).send({ status: false, message: "Token is incorrect" })
-        if (err.message == "invalid token") return res.status(400).send({ status: false, message: "Token is incorrect" })
-        
         res.status(500).send({ status: false, message: err.message });
     }
 }
